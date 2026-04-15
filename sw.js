15b144a9453a9e588e20fc539ad46458c96e8f8d
Atnan49 <atnan49@users.noreply.github.com>
2026-04-15 16:15:10 +0700

    Replace sw.js with the latest service worker

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10879673
+    "zoneId": 10879733
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
