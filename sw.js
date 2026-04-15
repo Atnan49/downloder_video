a44f021d8377b09dd10c862dde3094073534a7e4
Atnan49 <atnan49@users.noreply.github.com>
2026-04-15 16:40:23 +0700

    Add dual-zone sw.js config for testing

--- sw.js
+++ sw.js
@@ -1,6 +1,17 @@
-self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 10879673
-}
-self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+var monetagConfigs = {
+    first: {
+        domain: "3nbf4.com",
+        zoneId: 10879673
+    },
+    second: {
+        domain: "3nbf4.com",
+        zoneId: 10879733
+    }
+};
+
+// Pilih versi aktif: "first" atau "second"
+var activeConfigKey = "second";
+
+self.options = monetagConfigs[activeConfigKey];
+self.lary = "";
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw');
